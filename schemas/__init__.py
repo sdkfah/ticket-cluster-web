a8afc99b7b0e2fed96ff135d1db493f566152264
abfslg <sfjlsaf@gmail.com>
2026-02-06 15:56:42 +0800

    提交

--- schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-# schemas/__init__.py
-from .base import ResponseModel
-from .ticket import TicketItem
-from .task import OrderTask,OrderTaskCreate
-from .device import Device, DeviceGroup,GroupCreate,GroupUpdate,MigrateRequest
-from .log import ActionLog
--- schemas/base.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Any, Optional
-
-class ResponseModel(BaseModel):
-    code: int = 200
-    msg: str = "success"
-    data: Optional[Any] = None
